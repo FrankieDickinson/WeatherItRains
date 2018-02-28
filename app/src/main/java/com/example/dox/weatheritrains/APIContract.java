ee525ddeb06eb17f4197de41ad5fcb45cbfddeb2
Frankie Dickinson <kiwifrankiedox@gmail.com>
2018-03-01 12:38:35 +1300

    Removed API Key from repo and allows for unique API Keys

---
+++ app/src/main/java/com/example/dox/weatheritrains/APIContract.java
@@ -0,0 +1,11 @@
+package com.example.dox.weatheritrains;
+
+/**
+ * Created by dox on 1/03/18.
+ */
+
+// Interface which allows me to access an API key stored locally or on a server
+public interface APIContract {
+
+    String API_KEY = BuildConfig.MY_API_KEY;
+}
