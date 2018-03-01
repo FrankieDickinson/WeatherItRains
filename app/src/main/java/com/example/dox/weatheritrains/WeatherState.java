4d1fb79f974b612cc3782344eee2da6366249217
Frankie Dickinson <kiwifrankiedox@gmail.com>
2018-03-01 22:16:19 +1300

    WeatherState class returns Sting for a notification based on the current weather;

--- app/src/main/java/com/example/dox/weatheritrains/WeatherState.java
+++ app/src/main/java/com/example/dox/weatheritrains/WeatherState.java
@@ -7,11 +7,32 @@ package com.example.dox.weatheritrains;
 
 // Class which calculates which messages to display.
 public class WeatherState {
+    private Double precipitaion;
+    private Double temp;
 
-
-    public WeatherState(){
-
-
+    private final Double raining = 0.5;
+    private final int tempHot = 22;
+    
+    public WeatherState(Double precipitation, Double temp){
+        this.precipitaion = precipitation;
+        this.temp = temp;
 
     }
+
+    public String weatherStateMessage(){
+        String message;
+
+        if(temp > tempHot){
+            message = "It is hot";
+        } else {
+            message = "It is cold";
+        }
+
+        if(precipitaion >= raining){
+            message += "and it is going to rain!";
+        }
+
+        return message;
+    }
+
 }
